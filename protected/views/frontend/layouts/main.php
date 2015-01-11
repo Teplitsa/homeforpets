5cb54c66b542c103e357c9689365c0c6bc82400d
Dmitry Matveev <dimon9107@yandex.ru>
2015-01-11 21:50:18 +0300

    anim6

--- protected/views/frontend/layouts/main.php
+++ protected/views/frontend/layouts/main.php
@@ -1,14 +1,4 @@
 <?php $this->beginContent('//layouts/main_layout'); ?>
-<div class="inner">
-	<div class="breadcrumbs">
-	<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-		'homeLink' => CHtml::link('Главная','/'),
-		'separator' => ' <span> / </span> ',
-		'links' => $this->breadcrumbs,
-		));
-	?>
-	</div>
 	<?php echo $content; ?>
 	<div class="clear"></div>
-</div>
 <?php $this->endContent(); ?>
--- protected/views/frontend/layouts/main_layout.php
+++ protected/views/frontend/layouts/main_layout.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title><?php if ($this->title) echo CHtml::encode($this->title), ' - ', CHtml::encode(Yii::app()->config->sitename);?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
 	<meta name="keywords" content="<?php echo CHtml::encode($this->keywords); ?>"/>
 	<meta name="description" content="<?php echo CHtml::encode($this->description); ?>"/>
 	<meta name="language" content="ru" />
 	<meta name="author" content="<?php echo CHtml::encode(Yii::app()->config->author); ?>"/>
+	<title><?php if ($this->title) echo CHtml::encode($this->title), ' - ', CHtml::encode(Yii::app()->config->sitename);?></title>
 	<link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
 	<link href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" rel="stylesheet" type="text/css"/>
 </head>
@@ -15,36 +16,50 @@
 		<header>
 			<div class="inner">
 				<div class="top">
-					<a href="/" class="logo"><img src="/images/logo.png" alt=""/></a>
-					<div class="title"><span class="bullet"></span><?php echo CHtml::encode(Yii::app()->config->sitename); ?></div>
-					<div class="phones"><?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'telefony-v-shapke')); ?></div>
+					<div class="logo"><a href="/"><img src="/images/logo.png" alt=""/></a></div>
+					<div class="helps" title="Мы помогли найти дом 345 питомцам">
+						<div>Мы помогли найти дом 345 питомцам</div>
+					</div>
 				</div>
-			</div>
-			<div class="banners">
-				<div class="banner" style="background-image: url('/images/banner.jpg');"></div>
-			</div>
-			<div class="inner">
+				<div class="track-line"></div>
 				<div class="bot">
-					<div class="menu">
-						<?php $this->widget('application.widgets.OutMenu', array('name' => 'main')); ?>
+					<div class="bot-l">
+						<a href="#" class="s-btn">Я хочу помочь</a>
+						<a href="#" class="s-btn par">Предложить животное</a>
 					</div>
-					<div class="social">
-						<?php $this->widget('application.widgets.OutAreaWidget', array('name' => 'sotsseti-v-shapke')); ?>
+					<div class="bot-r">
+						<a href="#" class="sc-btn"><img src="/images/soc-in.png" alt=""/></a>
+						<a href="#" class="sc-btn"><img src="/images/soc-fb.png" alt=""/></a>
+						<a href="#" class="sc-btn"><img src="/images/soc-tw.png" alt=""/></a>
+						<a href="#" class="sc-btn"><img src="/images/soc-vk.png" alt=""/></a>
+						<a href="#" class="sc-btn"><img src="/images/soc-ok.png" alt=""/></a>
+						<a href="#" class="sc-sh-btn">Поделиться</a>
 					</div>
 				</div>
-				<div class="line"></div>
 			</div>
+			<div class="delta-line"></div>
 		</header>
 		<div class="content">
-			<?php echo $content; ?>
+			<div class="inner">
+				<?php echo $content; ?>
+			</div>
 		</div>
 		<div class="empty"></div>
 	</div>
 	<footer>
-		<div class="inner">
-			<a href="/" class="logo"><img src="/images/logo_footer.png" alt=""/></a>
-			<div class="title"><span class="bullet"></span><?php echo CHtml::encode(Yii::app()->config->sitename); ?></div>
-			<a href="http://plus1dev.ru/" class="dev"><img src="/images/logo_po.png" alt=""/></a>
+		<div class="delta-line r"></div>
+		<div class="footer_inner">
+			<div class="inner">
+				<div class="track-line"></div>
+				<div class="bot">
+					<div class="logo">
+						<a href="/"><img src="/images/logo.png" alt=""/></a>
+					</div>
+					<div class="dev">
+						<a href="http://plus1dev.ru/"><img src="/images/logo+1.png" alt=""/></a>
+					</div>
+				</div>
+			</div>
 		</div>
 	</footer>
 </body>
